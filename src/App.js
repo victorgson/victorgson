acc6dd511f0f47fe9f00f6afd695e04ffd5a7581
Victor <victorgson@outlook.com>
2022-03-02 22:59:19 +0100

    adds styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from "./pages/home";
 
 function App() {
   return (
     <div className="App">
-      <h1> Victor Gustafsson's portfolio</h1>
+      <Home />
     </div>
   );
 }
