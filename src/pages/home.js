8a07a6cf4870c56810330782a2e27a36e04ab617
Victor <victorgson@outlook.com>
2022-03-03 00:09:40 +0100

    add basic layout

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import "./home.css";
+import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io5";
+import { IconContext } from "react-icons";
 
 function home() {
+
   return (
-    <div>
-      <h1> Home </h1>
-      <p> asd </p>
-    </div>
+    <div className="main-container">
+      <h1> Victor Gustafsson </h1>
+      <p> Software Developer </p>
+
+
+      <div className="links-container">
+        <a href="https://github.com/victorgson" className="icon">
+          <IconContext.Provider value={{ style: { color: "white", height: "45px", width: "45px" } }}>
+            <div>
+              <IoLogoGithub />
+            </div>
+          </IconContext.Provider></a>
+        <a href="https://www.linkedin.com/in/victorgustafsson/" className="icon">
+
+          <IconContext.Provider value={{ style: { color: "white", height: "45px", width: "45px" } }}>
+            <div>
+              <IoLogoLinkedin />
+            </div>
+          </IconContext.Provider>
+        </a>
+      </div>
+    </div >
   )
 }
 
